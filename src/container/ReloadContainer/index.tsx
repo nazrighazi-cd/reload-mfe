2f1fb29f97cfa071ae8e3378fcb21eb3a4ad4623
nazri ghazi <nazri.ghazi@celcomdigi.com>
2023-05-23 14:23:43 +0800

    change copytext 'change amount'

--- src/container/ReloadContainer/index.tsx
+++ src/container/ReloadContainer/index.tsx
@@ -397,7 +397,7 @@ const ReloadContainer = (props: Props) => {
               {tabOption == 'reload' && (
                 <View>
                   <Text className="text-[12px] font-['Poppins-Regular'] text-[#333333]">
-                    Total Amount
+                    Total Amounts
                   </Text>
                   <Text className="font-semibold text-[22px] font-['Poppins-SemiBold'] text-[#333333]">
                     {selectedReload.title}
